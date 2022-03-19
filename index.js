6ac9ca0baf97ffec6ae4d2c4ea5358f87acd0c03
ramin harikan <harikan2007@gmail.com>
2022-03-19 12:38:47 +0330

    initial save 2

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const http = require("http");
 const port = 8080;
-const server = http.createServer((req, res)=>{
+const server = http.createServer();
+
+server.on("request", (req, res)=>{
+    if (req.url === "/friends"){
     res.writeHead(200, {
-        "Content-Type": "text/plain",
+        "Content-Type": "application/json",
     });
-    res.end(" hello! this is ramin.");
+    res.end(JSON.stringify({
+        id:1,
+        name: "ramin",
+        lastname:"choobineh"
+    }));} else if (req.url === "/messages"){
+        res.statusCode =200;
+        res.setHeader ("Content-Type","text/html");
+        res.write("<html>")
+        res.write("<body>")
+        res.write("<ul>")
+        res.write("<li> this ramin!</li>")
+        res.write("<li> this is the second message</li>")
+        res.write("</ul>")
+        res.write("</body>")
+        res.write("</html>")
+        res.end();
+    } else{
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
 server.listen(port, ()=>{
