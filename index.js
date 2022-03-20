8fa2fee633ffcfa7b8ed139cc2cfd180f314ba2d
ramin harikan <harikan2007@gmail.com>
2022-03-20 10:03:33 +0330

    basic http

--- index.js
+++ index.js
@@ -2,16 +2,43 @@ const http = require("http");
 const port = 8080;
 const server = http.createServer();
 
+const friends = [{
+    id:1,
+    name: "ramin",
+    lastname:"choobineh"
+},{
+    id:2,
+    name: "amin",
+    lastname:"choobineh"
+},{
+    id:3,
+    name: "negin",
+    lastname:"choobineh"
+},{
+    id:4,
+    name: "amir",
+    lastname:"choobineh"
+}]
 server.on("request", (req, res)=>{
-    if (req.url === "/friends"){
+    const items = req.url.split("/");
+    if (req.method === "POST" && items[1] === "friends"){
+        req.on("data", (data)=>{
+            friends.push(JSON.parse(data.toString()))
+        })
+        req.pipe(res)
+    }
+    else if (req.method === "GET" && items[1] === "friends"){
     res.writeHead(200, {
         "Content-Type": "application/json",
     });
-    res.end(JSON.stringify({
-        id:1,
-        name: "ramin",
-        lastname:"choobineh"
-    }));} else if (req.url === "/messages"){
+    if (items.length === 3){
+        const friendIndex = Number(items[2]);
+        res.end(JSON.stringify(friends[friendIndex]));
+    }else{
+        res.end(JSON.stringify(friends));
+    }
+    }
+     else if (items[1] === "messages"){
         res.statusCode =200;
         res.setHeader ("Content-Type","text/html");
         res.write("<html>")
